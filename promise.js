7a9e138b4ea6f07d7713f80a82969401302fe356
Nadeem Haider <nadeemzaidi514@gmail.com>
2022-05-18 16:36:52 +0500

    we modify the promise.js file

--- promise.js
+++ promise.js
@@ -5,14 +5,24 @@ url.then((item) => {
 }).then((get_Api_Id) => {
     for (var prop in get_Api_Id) {
         // object[prop]
-        this.list.push(get_Api_Id[prop])
+
+        list.push(get_Api_Id[prop])
         break;
         
     }
-    console.warn("Api's id list will be : " ,this.list[0][28].title)
+    console.warn("Api's id list will be : " ,list[0][28].title)
 }).catch((reject_error) => {
     alert("Rejection occur")
 })
 url.finally(() => {
   console.warn("promises works either resolve/reject");
 })
+setTimeout(()=>{
+    if (list===undefined) {
+        console.warn("the value of the api is not yet assigned")
+    }
+    else{
+        console.table(list[0])
+    }
+},3000)
+
